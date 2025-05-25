2a393b4d159d855448c379e268360facc2ede6bb
Tallyon Lima <142681972+Tallyon-Lima@users.noreply.github.com>
2025-05-25 17:38:19 -0300

    Ajuste na configurações do arquivo ci.yml

--- src/main/java/com/example/atividade3_sprint3/service/ConversorDolarService.java
+++ src/main/java/com/example/atividade3_sprint3/service/ConversorDolarService.java
@@ -1,11 +1,16 @@
 package com.example.atividade3_sprint3.service;
 
 import org.springframework.stereotype.Service;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Service
 public class ConversorDolarService {
 
-    public double converter(double valor){
-        return valor/5.65;
+    public double converter(double valor) {
+        BigDecimal resultado = BigDecimal.valueOf(valor)
+                .divide(BigDecimal.valueOf(5.65), 2, RoundingMode.HALF_UP);
+        return resultado.doubleValue();
     }
+
 }
--- src/test/java/com/example/atividade3_sprint3/service/ConversorDolarServiceTeste.java
+++ src/test/java/com/example/atividade3_sprint3/service/ConversorDolarServiceTeste.java
@@ -1,4 +1,20 @@
 package com.example.atividade3_sprint3.service;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ConversorDolarServiceTeste {
+
+    @Test
+    @DisplayName("Quando passado o valor 10 deve retornar 1,76")
+    public void testeConversorDolar(){
+        ConversorDolarService conversorDolarService = new ConversorDolarService();
+        Double valor = 10.0;
+
+        double resultado = conversorDolarService.converter(valor);
+
+        assertEquals(1.76, resultado);
+    }
 }
